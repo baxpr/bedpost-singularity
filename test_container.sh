ce1850fc88916cee55b3a32cea36d179acc14331
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-07-09 15:45:52 -0500

    test

---
+++ test_container.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+singularity run --contain --cleanenv \
+    --bind INPUTS:/tmp \
+    --bind INPUTS:/INPUTS \
+    --bind OUTPUTS:/OUTPUTS \
+    --label_info 'TEST LABEL' \
+    --dwi_niigz /INPUTS/*PREPROCESSED/dwmri.nii.gz \
+    --bvec_txt /INPUTS/*PREPROCESSED/dwmri.bvec \
+    --bval_txt /INPUTS/*PREPROCESSED/dwmri.bval \
+    --mask_niigz /INPUTS/*PREPROCESSED/mask.nii.gz \
+    --fa_niigz /INPUTS/*SCALARS/dwmri_tensor_fa.nii.gz \
+    --v1_niigz /INPUTS/*SCALARS/dwmri_tensor_v1.nii.gz \
+    --tensor_niigz /INPUTS/*TENSOR__dwmri_tensor.nii.gz \
+    --outdir /OUTPUTS
