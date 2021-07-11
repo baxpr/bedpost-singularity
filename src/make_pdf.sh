b15837cb9b678713e7632abd24a61b95807fbd08
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-07-11 09:16:47 -0500

    remove dti dependency

--- src/make_pdf.sh
+++ src/make_pdf.sh
@@ -16,16 +16,15 @@ dims="2400 1200"
 echo V1
 fsleyes render -of restore_v1.png \
     -hc -hl -xz 1200 -yz 1200 -zz 1200 --size ${dims} \
-    "${fa_niigz}" \
-    "${v1_niigz}" -ot linevector
+    dtifit/dti_FA \
+    dtifit/dti_V1 -ot linevector
 
-# TENSORS - fsleyes can't handle nans so we fix that first
+# TENSORS
 echo TENSORS
-fslmaths "${tensor_niigz}" -nan tensor
 fsleyes render -of restore_tensors.png \
-    -vl 64 65 45 --hidex --hidez -hc -hl -yz 1700 --size ${dims} \
-    "${fa_niigz}" \
-    tensor -ot tensor
+    --hidex --hidez -hc -hl -yz 1700 --size ${dims} \
+    dtifit/dti_FA \
+    dtifit/dti_tensor -ot tensor
 
 
 ### BEDPOSTX PNGs ###
@@ -48,7 +47,7 @@ fsleyes render -of bedpost_dyads_mod.png \
 # VECTOR MAP
 echo VECTOR MAP 1
 fsleyes render -of bedpost_vecs1.png \
-    -vl 64 65 45 --hidex --hidez -hc -hl -yz 1500 --size ${dims} \
+    --hidex --hidez -hc -hl -yz 1500 --size ${dims} \
     "${bedpost_dir}"/mean_fsumsamples \
     "${bedpost_dir}"/dyads1 \
         -ot linevector -xc 1 0 0 -yc 1 0 0 -zc 1 0 0 -lw 2 \
@@ -60,7 +59,7 @@ fsleyes render -of bedpost_vecs1.png \
 # VECTOR MAP
 echo VECTOR MAP 2
 fsleyes render -of bedpost_vecs2.png \
-    -vl 65 67 45 --hidex --hidey -hc -hl -zz 2000 --size ${dims} \
+    --hidex --hidey -hc -hl -zz 2000 --size ${dims} \
     "${bedpost_dir}"/mean_fsumsamples \
     "${bedpost_dir}"/dyads1 \
         -ot linevector -xc 1 0 0 -yc 1 0 0 -zc 1 0 0 -lw 2 \
@@ -77,7 +76,7 @@ convert \
 -size 2600x3365 xc:white \
 -gravity center \( bedpost_vecs1.png -resize 2400x1200 \) -geometry +0-650 -composite \
 -gravity center \( bedpost_vecs2.png -resize 2400x1200 \) -geometry +0+650 -composite \
--gravity center -pointsize 48 -annotate +0-1300 "Vector maps (BEDPOSTX)" \
+-gravity center -pointsize 48 -annotate +0-1300 "Dyad vectors (bedpostx)" \
 -gravity SouthEast -pointsize 48 -annotate +50+50 "$(date)" \
 -gravity NorthWest -pointsize 48 -annotate +50+50 "${info_string}" \
 page1.png
@@ -87,17 +86,17 @@ convert \
 -size 2600x3365 xc:white \
 -gravity center \( bedpost_dyads.png -resize 2400x1200 \) -geometry +0-650 -composite \
 -gravity center \( bedpost_dyads_mod.png -resize 2400x1200 \) -geometry +0+650 -composite \
--gravity center -pointsize 48 -annotate +0-1300 "Dyads (BEDPOSTX)" \
+-gravity center -pointsize 48 -annotate +0-1300 "First dyad (bedpostx)" \
 -gravity SouthEast -pointsize 48 -annotate +50+50 "$(date)" \
 -gravity NorthWest -pointsize 48 -annotate +50+50 "${info_string}" \
 page2.png
 
-# Page 3, DTI from RESTORE
+# Page 3, DTI
 convert \
 -size 2600x3365 xc:white \
 -gravity center \( restore_v1.png -resize 2400x1200 \) -geometry +0-650 -composite \
 -gravity center \( restore_tensors.png -resize 2400x1200 \) -geometry +0+650 -composite \
--gravity center -pointsize 48 -annotate +0-1300 "Tensor reconstruction (RESTORE)" \
+-gravity center -pointsize 48 -annotate +0-1300 "Tensor reconstruction (dtifit)" \
 -gravity SouthEast -pointsize 48 -annotate +50+50 "$(date)" \
 -gravity NorthWest -pointsize 48 -annotate +50+50 "${info_string}" \
 page3.png
--- src/pipeline.sh
+++ src/pipeline.sh
@@ -19,12 +19,6 @@ do
         export bvec_txt="$2"; shift; shift ;;
     --mask_niigz)
         export mask_niigz="$2"; shift; shift ;;
-    --fa_niigz)
-        export fa_niigz="$2"; shift; shift ;;
-    --v1_niigz)
-        export v1_niigz="$2"; shift; shift ;;
-    --tensor_niigz)
-        export tensor_niigz="$2"; shift; shift ;;
     --bedpost_params)
         export bedpost_params="$2"; shift; shift ;;
     --outdir)
@@ -41,9 +35,6 @@ echo "dwi_niigz:      $dwi_niigz"
 echo "bvec_txt:       $bvec_txt"
 echo "bval_txt:       $bval_txt"
 echo "mask_niigz:     $mask_niigz"
-echo "fa_niigz:       $fa_niigz"
-echo "v1_niigz:       $v1_niigz"
-echo "tensor_niigz:   $tensor_niigz"
 echo "bedpost params: $bedpost_params"
 echo "outdir:         $outdir"
 
@@ -53,6 +44,16 @@ cp "${dwi_niigz}" "${outdir}"/bedpostx/data.nii.gz
 cp "${mask_niigz}" "${outdir}"/bedpostx/nodif_brain_mask.nii.gz
 cp "${bval_txt}" "${outdir}"/bedpostx/bvals
 cp "${bvec_txt}" "${outdir}"/bedpostx/bvecs
+
+# Run dtifit for cross-check and viewing
+mkdir -p "${outdir}"/dtifit
+dtifit \
+    --save_tensor \
+    --out="${outdir}"/dtifit/dti \
+    --data="${outdir}"/bedpostx/data.nii.gz \
+    --bvecs="${outdir}"/bedpostx/bvecs \
+    --bvals="${outdir}"/bedpostx/bvals \
+    --mask="${outdir}"/bedpostx/nodif_brain_mask.nii.gz
 
 # Run bedpost
 bedpostx "${outdir}"/bedpostx ${bedpost_params}
