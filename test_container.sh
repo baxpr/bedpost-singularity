c00c83fce74429f64e6af2211a796fc6c7d46b46
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-10-07 12:58:32 -0500

    test

--- test_container.sh
+++ test_container.sh
@@ -4,7 +4,7 @@ singularity run --contain --cleanenv \
     --bind INPUTS:/tmp \
     --bind INPUTS:/INPUTS \
     --bind OUTPUTS:/OUTPUTS \
-    bedpost-singularity_v3.0.0-beta1.simg \
+    bedpost-singularity_v3.0.0-beta2.simg \
     --label_info 'TEST LABEL' \
     --dwi_niigz /INPUTS/PREPROCESSED/dwmri.nii.gz \
     --bvec_txt /INPUTS/PREPROCESSED/dwmri.bvec \
